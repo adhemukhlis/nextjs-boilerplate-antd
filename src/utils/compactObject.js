3846d70fff26c2e3efd64aaef593fee3923c6916
adhemukhlis <adhemukhlis@gmail.com>
2024-02-15 12:23:50 +0700

    add compactObject

---
+++ src/utils/compactObject.js
@@ -0,0 +1,10 @@
+const { pickBy, isNumber, isFinite, isBoolean, isEmpty } = require('lodash')
+/**
+ * Removes properties that contain falsy values from an object.
+ * @param {Object} obj - The object to compact.
+ * @param {boolean} [allowEmpty=false] - Whether to allow empty values.
+ * @returns {Object} - The compacted object.
+ */
+const compactObject = (obj, allowEmpty = false) =>
+	pickBy(obj, (v) => (isNumber(v) && isFinite(v)) || isBoolean(v) || !isEmpty(v) || allowEmpty)
+export default compactObject
